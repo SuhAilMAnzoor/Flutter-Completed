4c2e5c1973b5cd6ce8f23a705fb57afd73c3fe95
SuhailManzoor <sohailmanzoorsario@gmail.com>
2025-01-23 22:37:19 +0500

    Counter State, Listview, and ListView.Builder

---
+++ fourth_counter_state_update_listview_and_Listview_dot_Builder/fourth_counter_stateful_widget/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fourth_counter_stateful_widget/09.counter_view_List_Builder_with_add_item_using_TextField.dart';
+import 'package:fourth_counter_stateful_widget/10.counter_view_List_Builder_add_and_delete_item.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CounterViewNine(),
+    );
+  }
+}
