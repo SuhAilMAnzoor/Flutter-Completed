91fdda8ba66c7e40b89313a9102a6dec50a20974
SuhailManzoor <sohailmanzoorsario@gmail.com>
2025-01-31 17:08:43 +0500

    Gridview, Gridview.Builder,Navigation, Mediaqurey, Fonts

--- Sixth-Gridview-Gridview-dot-Builder-Navigation-Mediaqurey-Fonts/lib/fonts.dart
+++ Sixth-Gridview-Gridview-dot-Builder-Navigation-Mediaqurey-Fonts/lib/fonts.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:sixth_gridview_navigation_mediaqurey_fonts/font_custom_colors.dart';
+
+// fonts has 2 files Format TTF and OTF
 class Fonts extends StatelessWidget {
   const Fonts({super.key});
-
+// Media Qurey
   @override
   Widget build(BuildContext context) {
-    // MediaQuery to get screen height dynamically
-    final double spacing = MediaQuery.of(context).size.height * 0.02;
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: AppColors.appBarColor, // AppBar color from AppColors
-        title: const Text(
-          "My Fonts", // Title for AppBar
+        backgroundColor: Colors.blue,
+        title: Text(
+          "My Fonts",
+          style: TextStyle(fontFamily: "Tilt", fontWeight: FontWeight.w700),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: spacing), // Responsive spacing
-          const Center(
-            child: Text(
-              "This text is for test to check font style",
-              style: TextStyle(
-                fontSize: 16, // Example font size
-                color: AppColors.textColor, // Text color from AppColors
-              ),
-            ),
-          ),
-          const Text(
-            "Suhail Manzoor",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold, // Example bold style
-              color: AppColors.textColor,
-            ),
-          ),
-        ],
+      body: Container(
+        color: Colors.red,
+        height: MediaQuery.of(context).size.height * 0.5,
+        width: MediaQuery.of(context).size.width * 0.5,
+        // get the height and width of screen
       ),
     );
   }
