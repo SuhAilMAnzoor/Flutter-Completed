9ae8741948f35097a03aaf4e50090fe6adf06419
SuhAilMAnzoor <Sohailmanzoorsario@gmail.com>
2025-02-24 20:41:32 +0500

    Added First Screen, Used ElevatedButton For Navigation to 2nd Screen

---
+++ Sixth-Navigation-another-Example-Part-Three/lib/first_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sixth_navigation_another_example/second_screen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text("Go to 2nd Screen"),
+        ),
+      ),
+    );
+  }
+}
