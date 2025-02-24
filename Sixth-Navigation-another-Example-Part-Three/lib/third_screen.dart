1ed44829acded38248af9ab81a56941e9bb16dbf
SuhAilMAnzoor <Sohailmanzoorsario@gmail.com>
2025-02-24 20:51:18 +0500

    When you hit back button, You will be at first screen not Second Screen , because pushReplacement is used in second screen, check second_screen for code, pushReplacement is used in 2nd Screen that means, that second screen is not remebered in routes it will navigate to firstScreen it only record the previous route not

---
+++ Sixth-Navigation-another-Example-Part-Three/lib/third_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Third Screen"),
+      ),
+      body: const Text("When you back, You will be at first screen not Second Screen , because pushReplacement is used in second screen, check second_screen for code"),
+    );
+  }
+}
