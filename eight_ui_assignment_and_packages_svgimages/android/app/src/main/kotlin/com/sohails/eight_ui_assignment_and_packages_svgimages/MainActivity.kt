081a0aa100306f81f8ba4a4bde6a411c7bb9ec7e
Suhail Manzoor <93788026+SuhAilMAnzoor@users.noreply.github.com>
2025-06-11 10:51:49 +0500

    Delete eight_ui_assignment_and_packages_svgimages directory

--- eight_ui_assignment_and_packages_svgimages/android/app/src/main/kotlin/com/sohails/eight_ui_assignment_and_packages_svgimages/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sohails.eight_ui_assignment_and_packages_svgimages
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- eight_ui_assignment_and_packages_svgimages/lib/previous_pages/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors{
-  static Color orangeColor = Color(0xffFFA451);
-
-}
